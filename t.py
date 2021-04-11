9d9187c882b95f3b3836339b90992e5c503472f1
m32 <grzegorz.makarewicz@gmail.com>
2021-04-12 00:29:07 +0200

    logging/username+userpass

---
+++ t.py
@@ -0,0 +1,11 @@
+import logging
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger('sqlalchemy').setLevel(logging.DEBUG)
+#logging.getLogger('pytds').setLevel(logging.DEBUG)
+#logging.getLogger('pytds.__init__').setLevel(logging.DEBUG)
+#logging.getLogger('pytds.tds').setLevel(logging.DEBUG)
+
+username = 'sa'
+userpass = 'SaAdmin1@'
