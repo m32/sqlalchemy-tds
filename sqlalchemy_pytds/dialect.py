083611d62483f3f85f49e0e3144dc3f36f0dabdb
m32 <grzegorz.makarewicz@gmail.com>
2024-08-30 21:59:24 +0200

    proper type for python bytes type

--- sqlalchemy_pytds/dialect.py
+++ sqlalchemy_pytds/dialect.py
@@ -175,7 +175,7 @@ class MSDialect_pytds(PyTDSConnector, MSDialect):
                             parameters[name] = tds_base.Param(name=name, type=tds_types.NVarCharType(size=len(v)), value=v, flags=0)
                         elif isinstance(v, bytes):
                             assert len(v) < 8000
-                            parameters[name] = tds_base.Param(name=name, type=tds_types.NVarCharType(size=len(v)), value=v, flags=0)
+                            parameters[name] = tds_base.Param(name=name, type=tds_types.VarBinaryType(size=len(v)), value=v, flags=0)
             #print('parm:', parameters)
         cursor.execute(statement, parameters)
 
